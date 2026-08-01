e2b7f3d90e729d488a917437252576cd37ae6b9f
ArchitectureWorld <140295054+ArchitectureWorld@users.noreply.github.com>
2026-08-01 12:15:46 +0800

    feat: publish reviewed plugin identity as v0.7.0

--- src/BIMBaoGui.Stage01/AssemblyInfo.cs
+++ src/BIMBaoGui.Stage01/AssemblyInfo.cs
@@ -9,10 +9,11 @@ namespace BIMBaoGui.Stage01
   {
     public override string Name => "BIMBaoGui.Stage01";
     public override Bitmap Icon => IconFactory.CreateComponentIcon();
-    public override string Description => "湖北省 BIM 规划报建：Revit 2020 文件初始化与模型任务分流组件。";
+    public override string Description =>
+      "湖北省 BIM 规划报建：文件初始化、模型任务分流与官方插件候选兼容属性写入。";
     public override Guid Id => new Guid("9f7b1bd4-fb6a-4853-b60f-6576193e1601");
     public override string AuthorName => "ArchitectureWorld";
     public override string AuthorContact => "ArchitectureWorld/BIM-baogui";
-    public override string Version => "0.5.0";
+    public override string Version => "0.7.0";
   }
 }
